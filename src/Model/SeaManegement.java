6df27b53bc25234068f9569fa29f6c95de2aabb2
Ronaldo jose <ronaldo.jrk@gmail.com>
2019-06-11 12:50:34 -0300

    joguei 1

--- src/Model/SeaManegement.java
+++ src/Model/SeaManegement.java
@@ -14,16 +14,16 @@ import java.util.ArrayList;
 public class SeaManegement {
 
     public static void main(String[] args) {
-       /* Login log = new Login();
+        Login log = new Login();
         log.setVisible(true);
         Costureiro cost = new Costureiro();
         Controller control = new Controller(log, cost);
        log.setVisible(true);
        log.setLocationRelativeTo(null);
-        */                        
+                                
        
        
-        
+        /*
         
         // fornecedor
         ArrayList<Fornecedor>fornecedores = new ArrayList<>();
